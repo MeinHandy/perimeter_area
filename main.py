49befc58f7825362de4ac4bf80536cd547d5f151
ah19017 <ah19017@melville-high.school.nz>
2022-11-09 12:50:35 +1300

    decided on using a single file, missed many push opportunities, most of the code has been copy/pasted and modified to have a better purpose. Code should be refined.

--- main.py
+++ main.py
@@ -1,16 +1,71 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import math as m
+supported_shapes = ["square", "circle", "rectangle", "parallelogram"]
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def circle_dimensions():
+    invalid = True
+    while invalid:
+        try:
+            radius = float(input("What is the radius of circle? "))
+            invalid = False
+            area = m.pi * radius ** 2
+            circumference = m.pi * (2 * radius)
+        except ValueError:
+            print("Please input a number")
+    print("Circle area: {:.2f}, circle circumference: {:.2f} (2 decimal places)".format(area, circumference))
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def square_dimensions():
+    invalid = True
+    while invalid:
+        try:
+            side = float(input("What is a side dimension? "))
+            invalid = False
+            area = side ** 2
+            perimeter = side * 4
+        except ValueError:
+            print("Please input a number")
+    print("Square area: {:.2f}, Square perimeter: {:.2f} (2 decimal places)".format(area, perimeter))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def rectangle_dimensions():
+    invalid = True
+    while invalid:
+        try:
+            side0 = float(input("What is the first side dimension? "))
+            side1 = float(input("What is the second side dimension? "))
+            invalid = False
+            area = side0 * side1
+            perimeter = side0 * 2 + side1 * 2  # the math shouldn't be in the try/except but will be fixed later cause lazy
+        except ValueError:
+            print("Please input a number")
+    print("Rectangle area: {:.2f}, Rectangle perimeter: {:.2f} (2 decimal places)".format(area, perimeter))
+
+
+def parallelogram_dimensions():
+    invalid = True
+    while invalid:
+        try:
+            side0 = float(input("What is the first side dimension? "))
+            side1 = float(input("What is the second side dimension? "))
+            invalid = False
+            area = side0 * side1
+            perimeter = side0 * 2 + side1 * 2  # the math shouldn't be in the try/except but will be fixed later cause lazy
+        except ValueError:
+            print("Please input a number")
+    print("parallelogram area: {:.2f}, parallelogram perimeter: {:.2f} (2 decimal places)".format(area, perimeter))
+
+
+while True:
+    shape_request = input("What shape dimensions of the following: {}".format(supported_shapes))
+    if shape_request not in supported_shapes:
+        print("Invalid shape")
+        continue
+    elif shape_request == "circle":
+        circle_dimensions()
+    elif shape_request == "square":
+        square_dimensions()
+    elif shape_request == "rectangle":
+        rectangle_dimensions()
+    elif shape_request == "parallelogram":
+        parallelogram_dimensions()
