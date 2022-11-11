b196057124d43dc2f0bfe03d61117c6df93a6580
ah19017 <ah19017@melville-high.school.nz>
2022-11-11 14:55:34 +1300

    added a try/except function to streamline development and started working on the triangle dimensions subroutine

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 import math as m
-supported_shapes = ["square", "circle", "rectangle", "parallelogram"]
+supported_shapes = ["square", "circle", "rectangle", "parallelogram", "triangle"]
 
+
+def try_except(question):  # a function for try/except statements considering they are so highly used
+    invalid = True
+    while invalid:
+        try:
+            answer = float(input(question))
+            invalid = False
+        except ValueError:
+            print("Please input a number")
+    return answer  # this is the most useful function ever
 
 def circle_dimensions():
     invalid = True
@@ -13,14 +23,7 @@ def circle_dimensions():
             continue
         else:
             print("Error, please choose between 'diameter' or 'radius'.")
-
-    invalid = True
-    while invalid:
-        try:
-            dimension = float(input("What is the {} of the circle? \n".format(dimension_provided)))
-            invalid = False
-        except ValueError:
-            print("Please input a number")
+    dimension = try_except("What is the radius/diameter of the circle? \n")  # try/except for circle dimension
     if dimension_provided == "diameter":
         radius = dimension / 2
     elif dimension_provided == "radius":
@@ -31,48 +34,44 @@ def circle_dimensions():
 
 
 def square_dimensions():
-    invalid = True
-    while invalid:
-        try:
-            side = float(input("What is a side dimension? \n"))
-            invalid = False
-        except ValueError:
-            print("Please input a number")
+    side = try_except("What is a side dimension? \n")
     area = side ** 2
     perimeter = side * 4
     print("Square area: {:.2f}, Square perimeter: {:.2f} (2 decimal places)".format(area, perimeter))
 
 
 def rectangle_dimensions():
-    invalid = True
-    while invalid:
-        try:
-            side0 = float(input("What is the first side dimension? "))
-            side1 = float(input("What is the second side dimension? "))
-            invalid = False
-        except ValueError:
-            print("Please input a number")
+    side0 = try_except("What is the first side dimension? ")
+    side1 = try_except("What is the second side dimension? ")
     area = side0 * side1
     perimeter = side0 * 2 + side1 * 2
     print("Rectangle area: {:.2f}, Rectangle perimeter: {:.2f} (2 decimal places)".format(area, perimeter))
 
 
 def parallelogram_dimensions():
-    invalid = True
-    while invalid:
-        try:
-            side0 = float(input("What is the first side dimension? "))
-            side1 = float(input("What is the second side dimension? "))
-            invalid = False
-        except ValueError:
-            print("Please input a number")
+    side0 = try_except("What is the first side dimension? ")
+    side1 = try_except("What is the second side dimension? ")
     area = side0 * side1
     perimeter = side0 * 2 + side1 * 2
     print("parallelogram area: {:.2f}, parallelogram perimeter: {:.2f} (2 decimal places)".format(area, perimeter))
 
 
+def triangle_dimensions():
+    sides = []
+    side_count = int(try_except("How many sides are provided?\n"))  # try_except returns a float, for loops do not like floats, so it is converted into an integer.
+    if side_count > 3:
+        print("Triangle cannot have >3 sides, that shape is not a triangle.")
+        pass
+    for i in range(side_count):
+        side = try_except("What is the dimension of a side? \n")
+        sides.append(side)
+    if side_count >= 2:
+        area = sides[0] * sides[1] * 0.5  # (1/2) base * height, but really just half the area of a square.
+    print("triangle area: {:.2}".format(area))
+
+
 while True:
-    shape_request = input("What shape dimensions need solving of the following: {}. Type 'exit' to exit the program. \n".format(supported_shapes))
+    shape_request = input("What shape dimensions need solving of the following: {}. Type 'exit' to exit the program. \n".format(supported_shapes)).strip().lower()
     if shape_request == "circle":  # if/many elif to take the input and direct the program/user in the right direction
         circle_dimensions()
     elif shape_request == "square":
@@ -81,6 +80,8 @@ while True:
         rectangle_dimensions()
     elif shape_request == "parallelogram":
         parallelogram_dimensions()
+    elif shape_request == "triangle":
+        triangle_dimensions()
     elif shape_request == "exit":
         exit("Manual program exit")
     else:
