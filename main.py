9e93b29a511b8f30690a3e3470e745e4ce77fb9c
ah19017 <ah19017@melville-high.school.nz>
2022-11-21 10:08:53 +1300

    restarted triangle_dimensions(), when 3 sides and 2 sides given are completed for functionality purposes.

--- main.py
+++ main.py
@@ -58,51 +58,36 @@ def parallelogram_dimensions():
 
 
 def triangle_dimensions():
-    sides = []
-    side_count = int(try_except("How many sides are provided?\n"))  # try_except returns a float, for loops do not like floats, so it is converted into an integer.
-    if side_count > 3:
-        print("Triangle cannot have >3 sides, that shape is not a triangle.")
-    for i in range(side_count):
-        side = try_except("What is the dimension of a side? \n")
-        sides.append(side)
-    if side_count == 3:
-        if sides[0] > sides[1] and sides[2]:  # if/elif identifies the hypotenuse, to adjust calculations to exclude it
-            area = sides[1] * sides[2] * 0.5  # formula for area of right-angled triangle
-        elif sides[1] > sides[2] and sides[0]:
-            area = sides[2] * sides[0] * 0.5
-        elif sides[2] > sides[0] and sides[1]:
-            area = sides[0] * sides[1] * 0.5
-        else:  # because one side isn't longer, this is not a valid triangle
-            print("Invalid triangle")
-        # perimeter = sides[0] + sides[1] + sides[2]
-    elif side_count == 2:  # 2 sides means pythagoras is required
+    method = int(try_except("How many dimension(s) are known?"))
+    if method == 3:  # no advanced triangle calculations needed
+        hypotenuse = float(try_except("What is the dimension of the hypotenuse?"))
+        side_a = float(try_except("What is the dimension of side A?"))
+        side_b = float(try_except("What is the dimension of side B?"))
+        area = 0.5 * side_b * side_a
+        perimeter = hypotenuse + side_b + side_a
+        print("perimeter", perimeter, ". area", area)
+    elif method == 2:  # pythagoras required
         invalid = True
         while invalid:
-            unknown = input("What side is unaccounted for? hypotenuse, opposite or adjacent?")  # this is to adjust pythagoras formula as needed
+            unknown = input("What dimension is UNKNOWN? (hypotenuse/side A/side B)").lower()
             if unknown == "hypotenuse":
-                hypotenuse = m.sqrt(sides[0] ** 2 + sides[1] ** 2)
-                area = sides[0] * sides[1] * 0.5
-                sides.append(hypotenuse)
+                side_a = float(try_except("What is the dimension of side A?"))
+                side_b = float(try_except("What is the dimension of side B"))
+                hypotenuse = m.sqrt(side_a ** 2 + side_b ** 2)
+                print("Hypotenuse = {}".format(hypotenuse))
                 invalid = False
-            elif unknown == "opposite" or "adjacent":  # to know which side is the longest; the hypotenuse
-                if sides[0] > sides[1]:
-                    hypotenuse = sides[0]  # if sides[0] is larger, it has to be the hypotenuse
-                    other = sides[1]
-                elif sides[1] > sides   [0]:
-                    hypotenuse = sides[1]  # if sides[0] is not larger, sides[1] is the hypotenuse; double checks
-                    other = sides[0]
-                else:
-                    print("Error hypotenuse is equal to opposite/adjacent")
-
-                moment = m.sqrt(hypotenuse ** 2 - other ** 2)
-                area = moment * other * 0.5  # area of triangle cannot use hypotenuse
-                sides.append(moment)
+            elif unknown == "side a" or unknown == "side b":
+                hypotenuse = float(try_except("What is the dimension of the hypotenuse?"))
+                side_a = float(try_except("What is the dimension of side A?"))
+                side_b = m.sqrt(hypotenuse ** 2 - side_a ** 2)
+                print("Unknown side = {}".format(side_b))
                 invalid = False
             else:
-                print("Please input 'hypotenuse', 'opposite', 'adjacent'.")
-        # area = sides[0] * sides[1] * 0.5  # (1/2) base * height, but really just half the area of a square.
-    perimeter = sides[0] + sides[1] + sides[2]
-    print("triangle area: {:.2f}, perimeter {:.2f}".format(area, perimeter))
+                print("Error, please input hypotenuse/side A/side B")
+        area = 0.5 * side_a * side_b
+        perimeter = hypotenuse + side_b + side_a
+        print("perimeter", perimeter, ". area", area)
+
 
 
 while True:  # loops the program until intentional exit
