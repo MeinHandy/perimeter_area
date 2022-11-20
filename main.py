b6bb3ddce8a16aacbd189211774f69b5a738e565
ah19017 <ah19017@melville-high.school.nz>
2022-11-21 09:28:38 +1300

    code functionality unknown, code functionality has been improved

--- main.py
+++ main.py
@@ -66,32 +66,46 @@ def triangle_dimensions():
         side = try_except("What is the dimension of a side? \n")
         sides.append(side)
     if side_count == 3:
-        area = sides[0] * sides[1] * 0.5  # (1/2) base * height, but really just half the area of a square.
-        perimeter = sides[0] + sides[1] + sides[2]
-    if side_count == 2:
-        unknown = input("What side is unaccounted for? hypotenuse, opposite or adjacent?")  # this is to adjust pythagoras formula as needed
-        if unknown == "hypotenuse":
-            hypotenuse = m.sqrt(sides[0] ** 2 + sides[1] ** 2)
-            sides.append(hypotenuse)
-        elif unknown == "opposite" or "adjacent":  # to know which side is the longest; the hypotenuse
-            if sides[0] > sides[1]:
-                hypotenuse = sides[0]  # if sides[0] is larger, it has to be the hypotenuse
-                other = sides[1]
-            else:
-                hypotenuse = sides[1]  # if sides[0] is not larger, sides[1] is the hypotenuse
-                other = sides[0]
+        if sides[0] > sides[1] and sides[2]:  # if/elif identifies the hypotenuse, to adjust calculations to exclude it
+            area = sides[1] * sides[2] * 0.5  # formula for area of right-angled triangle
+        elif sides[1] > sides[2] and sides[0]:
+            area = sides[2] * sides[0] * 0.5
+        elif sides[2] > sides[0] and sides[1]:
+            area = sides[0] * sides[1] * 0.5
+        else:  # because one side isn't longer, this is not a valid triangle
+            print("Invalid triangle")
+        # perimeter = sides[0] + sides[1] + sides[2]
+    elif side_count == 2:  # 2 sides means pythagoras is required
+        invalid = True
+        while invalid:
+            unknown = input("What side is unaccounted for? hypotenuse, opposite or adjacent?")  # this is to adjust pythagoras formula as needed
+            if unknown == "hypotenuse":
+                hypotenuse = m.sqrt(sides[0] ** 2 + sides[1] ** 2)
+                area = sides[0] * sides[1] * 0.5
+                sides.append(hypotenuse)
+                invalid = False
+            elif unknown == "opposite" or "adjacent":  # to know which side is the longest; the hypotenuse
+                if sides[0] > sides[1]:
+                    hypotenuse = sides[0]  # if sides[0] is larger, it has to be the hypotenuse
+                    other = sides[1]
+                elif sides[1] > sides[0]:
+                    hypotenuse = sides[1]  # if sides[0] is not larger, sides[1] is the hypotenuse; double checks
+                    other = sides[0]
+                else:
+                    print("Error hypotenuse is equal to opposite/adjacent")
 
-            moment = m.sqrt(hypotenuse ** 2 - other ** 2)
-            area = moment * other * 0.5  # area of triangle cannot use hypotenuse
-            sides.append(moment)
-        else:
-            print("Please input 'hypotenuse', 'opposite', 'adjacent'.")
+                moment = m.sqrt(hypotenuse ** 2 - other ** 2)
+                area = moment * other * 0.5  # area of triangle cannot use hypotenuse
+                sides.append(moment)
+                invalid = False
+            else:
+                print("Please input 'hypotenuse', 'opposite', 'adjacent'.")
         # area = sides[0] * sides[1] * 0.5  # (1/2) base * height, but really just half the area of a square.
-        perimeter = sides[0] + sides[1] + sides[2]
+    perimeter = sides[0] + sides[1] + sides[2]
     print("triangle area: {:.2f}, perimeter {:.2f}".format(area, perimeter))
 
 
-while True:
+while True:  # loops the program until intentional exit
     shape_request = input("What shape dimensions need solving of the following: {}. Type 'exit' to exit the program. \n".format(supported_shapes)).strip().lower()
     if shape_request == "circle":  # if/many elif to take the input and direct the program/user in the right direction
         circle_dimensions()
